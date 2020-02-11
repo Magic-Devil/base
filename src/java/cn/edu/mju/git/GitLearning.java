41678d1ebdec3558ed203f1f29b6c050d75d7f4c
lx <2807677667@qq.com>
2020-02-11 17:53:30 +0800

    first commit

---
+++ src/java/cn/edu/mju/git/GitLearning.java
@@ -0,0 +1,14 @@
+package cn.edu.mju.git;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author LX
+ */
+public class GitLearning {
+
+    @Test
+    public void test() {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+    }
+}
